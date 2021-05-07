cecb80fa9580e4b1614ca4687596c7af182a21ec
JeanPCordeiro <jeanpierre.cordeiro@gmail.com>
2021-05-07 18:02:19 +0000

    fix init data

--- .gitpod.init.sh
+++ .gitpod.init.sh
@@ -2,7 +2,7 @@ while !(mysqladmin ping --silent); do
     sleep 1
 done
 mysql -u root -e "create database vaccin"
-mysql -u root vaccin < init_data.sql
+#mysql -u root vaccin < init_data.sql
 cp .env.example .env
 sed -i "s|APP_URL=|APP_URL=${GITPOD_WORKSPACE_URL}|g" .env
 sed -i "s|APP_URL=https://|APP_URL=https://8000-|g" .env
